25238dc0a0da3bc3dfb5a88de7c62c43b067d8ee
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2023-02-14 14:22:09 +0800

    ssef finals lesgo

--- index.js
+++ index.js
@@ -728,7 +728,8 @@ const SSEF = Vue.extend({
         {text: "Title", value: "title"},
         {text: "School", value: "school"},
         {text: "Team Leader", value: "teamLeader"},
-        {text: "Last Updated", value: "lastUpdated", filterable: false}]
+        // {text: "Last Updated", value: "lastUpdated", filterable: false},
+        {text: "Status", value: "status"}]
     };
   },
     async mounted() {
@@ -751,14 +752,15 @@ const SSEF = Vue.extend({
             data = data.slice(start, end).replace("\t", "");
             start = data.indexOf("[");
             end = data.lastIndexOf("]")+1;
-            data = JSON.parse(data.slice(start, end)).filter((it) => it.status == 20).map(
+            data = JSON.parse(data.slice(start, end)).filter((it) => it.status == 20 || it.status == 60).map(
                 (object) => {
                     return {
                         projectCode: object.category1 + object.projectCode.toString().padStart(3, "0"),
                         lastUpdated: moment(object.submissionDate, "DD/MM/YYYY hh:mm").toDate(),
                         teamLeader: object.memberName,
                         school: object.school.replace("&#39", "'"),
-                        title: object.title.replace("&#39", "'")
+                        title: object.title.replace("&#39", "'"),
+                        status: object.status == 20 ? "Not In Final": "Final"
                     }
                 }
             );
